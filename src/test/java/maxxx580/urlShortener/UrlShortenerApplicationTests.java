19b918b1a1a9eca7c833a8122ce4c8a6fd6e0662
Eric Ma <ericma0824@gmail.com>
2019-12-28 19:10:29 -0500

    added dependencies

--- src/test/java/maxxx580/urlShortener/UrlShortenerApplicationTests.java
+++ src/test/java/maxxx580/urlShortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,9 @@
 package maxxx580.urlShortener;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
